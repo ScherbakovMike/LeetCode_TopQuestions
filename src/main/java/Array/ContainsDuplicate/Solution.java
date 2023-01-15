8e3bae709ee2a04ecefdb9753f3f6df1553b8151
ScherbakovMike <62207753+ScherbakovMike@users.noreply.github.com>
2023-01-15 19:00:48 +0300

    Task 4

---
+++ src/main/java/Array/ContainsDuplicate/Solution.java
@@ -0,0 +1,21 @@
+package Array.ContainsDuplicate;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+class Solution {
+  public boolean containsDuplicate(int[] nums) {
+    return Arrays.stream(nums)
+        .boxed()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+        .entrySet().stream()
+        .anyMatch(entry -> entry.getValue() > 1);
+  }
+
+  public static void main(String[] args) {
+    var test = new int[]{1, 2, 3, 4};
+    System.out.println(new Solution().containsDuplicate(test));
+  }
+
+}
