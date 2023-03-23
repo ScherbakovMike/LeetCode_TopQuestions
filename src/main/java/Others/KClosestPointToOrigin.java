3153c488f81c0ee4e5f673a0ee19e5bd952c4c18
Scherbakov Mike <scherbakov.mike@gmail.com>
2023-03-23 21:36:46 +0300

    KClosestPointToOrigin task solution

---
+++ src/main/java/Others/KClosestPointToOrigin.java
@@ -0,0 +1,23 @@
+package Others;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class KClosestPointToOrigin {
+    public static void main(String[] args) {
+        var points = new int[][]{{3, 3}, {5, -1}, {-2, 4}};
+        var k = 2;
+        System.out.println(Arrays.deepToString(new KClosestPointToOrigin().kClosest(points, k)));
+    }
+
+    public int[][] kClosest(int[][] points, int k) {
+        return IntStream.range(0, points.length)
+                .boxed()
+                .sorted(Comparator.comparingInt(xIndex -> points[xIndex][0] * points[xIndex][0] + points[xIndex][1] * points[xIndex][1]))
+                .limit(k)
+                .map(xIndex -> points[xIndex])
+                .toList()
+                .toArray(new int[0][0]);
+    }
+}
