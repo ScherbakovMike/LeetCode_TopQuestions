66ca42d7c956ac5f8a2e256bbf7a841d5123dea8
ScherbakovMike <scherbakov.mike@gmail.com>
2023-06-29 18:24:40 +0300

    Delete node in a linked list (started)

---
+++ src/main/java/LinkedLists/DeleteNodeInALinkedList/Solution.java
@@ -0,0 +1,16 @@
+package LinkedLists.DeleteNodeInALinkedList;
+
+public class Solution {
+    public void deleteNode(ListNode node) {
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
