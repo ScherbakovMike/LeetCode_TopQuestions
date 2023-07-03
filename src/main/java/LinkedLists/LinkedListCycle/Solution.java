3546bea1c00f64e428d0ecf19e87bfcd1c57e1ea
ScherbakovMike <scherbakov.mike@gmail.com>
2023-07-03 21:00:29 +0300

    The solution of task "LinkedListCycle" has been implemented.

---
+++ src/main/java/LinkedLists/LinkedListCycle/Solution.java
@@ -0,0 +1,53 @@
+package LinkedLists.LinkedListCycle;
+
+import java.util.HashSet;
+
+public class Solution {
+  public boolean hasCycle(ListNode head) {
+    if (head == null) return false;
+    var left = head;
+    var links = new HashSet<ListNode>();
+    while (!links.contains(left.next) && left.next != null) {
+      links.add(left);
+      left = left.next;
+    }
+    return (links.contains(left.next));
+  }
+
+  private static void printList(ListNode head) {
+    var currentNode = head;
+    while (currentNode != null) {
+      System.out.println(currentNode.val);
+      currentNode = currentNode.next;
+    }
+  }
+
+  public static void main(String[] args) {
+    ListNode head = new ListNode(0);
+    head.next = new ListNode(1);
+    head.next.next = new ListNode(2);
+    head.next.next.next = new ListNode(3);
+    head.next.next.next.next = head.next;
+
+    //head.next.next.next.next = head.next;
+    System.out.println(new Solution().hasCycle(head));
+  }
+
+}
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
