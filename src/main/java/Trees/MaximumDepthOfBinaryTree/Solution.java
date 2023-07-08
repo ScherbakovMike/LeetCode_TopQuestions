dd093443cddc5e3c4d39470e4e281751899522a1
ScherbakovMike <scherbakov.mike@gmail.com>
2023-07-08 14:50:51 +0300

    The task 'Max depth of tree' has been improved.

--- src/main/java/Trees/MaximumDepthOfBinaryTree/Solution.java
+++ src/main/java/Trees/MaximumDepthOfBinaryTree/Solution.java
@@ -1,57 +1,55 @@
 package Trees.MaximumDepthOfBinaryTree;
 
+import com.sun.source.tree.Tree;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 //Definition for a binary tree node.
 class TreeNode {
-  int val;
-  TreeNode left;
-  TreeNode right;
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-  TreeNode() {
-  }
+    TreeNode() {
+    }
 
-  TreeNode(int val) {
-    this.val = val;
-  }
+    TreeNode(int val) {
+        this.val = val;
+    }
 
-  TreeNode(int val, TreeNode left, TreeNode right) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
 
 public class Solution {
-  public int maxDepth(TreeNode root) {
-    AtomicInteger maxDepth = new AtomicInteger(0);
-    AtomicInteger currentDepth = new AtomicInteger(0);
-    traversalTreeInDepth(root, maxDepth, currentDepth);
-    return maxDepth.get();
-  }
-
-  private void traversalTreeInDepth(TreeNode root, AtomicInteger maxDepth, AtomicInteger currentDepth) {
-    if (root != null) {
-      currentDepth.incrementAndGet();
-      var currentDepthBackup = new AtomicInteger(currentDepth.get());
-      traversalTreeInDepth(root.left, maxDepth, currentDepth);
-      if (currentDepth.get() > maxDepth.get()) {
-        maxDepth.set(currentDepth.get());
-      }
-      currentDepth.set(currentDepthBackup.get());
-      traversalTreeInDepth(root.right, maxDepth, currentDepth);
-      if (currentDepth.get() > maxDepth.get()) {
-        maxDepth.set(currentDepth.get());
-      }
+    public int maxDepth(TreeNode root) {
+        return traversalTreeInDepth(root, 0);
     }
-  }
 
-  public static void main(String[] args) {
-    var tree = new TreeNode(3,
-      new TreeNode(9, null, null),
-      new TreeNode(20,
-        new TreeNode(15, null, null),
-        new TreeNode(7, null, null)));
-    System.out.println(new Solution().maxDepth(tree));
-  }
+    private int traversalTreeInDepth(TreeNode root, int currentDepth) {
+        if (root != null) {
+            currentDepth++;
+            return Math.max(traversalTreeInDepth(root.left, currentDepth),
+                    traversalTreeInDepth(root.right, currentDepth));
+        }
+        return currentDepth;
+    }
+
+    public static void main(String[] args) {
+        var tree = new TreeNode(3,
+                new TreeNode(9,
+                        new TreeNode(10,
+                                new TreeNode(11,
+                                        new TreeNode(12, null, null),
+                                        null),
+                                null),
+                        null),
+                new TreeNode(20,
+                        new TreeNode(15, null, null),
+                        new TreeNode(7, null, null)));
+        System.out.println(new Solution().maxDepth(tree));
+    }
 }
