f0ab2e227a9df3bd69ad68f1670a9f4df3b12153
ScherbakovMike <scherbakov.mike@gmail.com>
2023-08-12 14:02:09 +0300

    The task "Merge sorted array" has been implemented.

---
+++ src/main/java/LinkedInTop/MergeSortedArray.java
@@ -0,0 +1,29 @@
+package LinkedInTop;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+  public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    int i = m - 1;
+    int j = n - 1;
+    int pos = nums1.length - 1;
+    while (pos >= 0) {
+      if (i >= 0 && j >= 0 && nums1[i] >= nums2[j]) {
+        nums1[pos] = nums1[i];
+        i--;
+      } else if (j >= 0) {
+        nums1[pos] = nums2[j];
+        j--;
+      }
+      pos--;
+    }
+  }
+
+  public static void main(String[] args) {
+    var nums1 = new int[]{1};
+    var nums2 = new int[]{};
+    merge(nums1, 1, nums2, 0);
+
+    System.out.println(Arrays.toString(nums1));
+  }
+}
