c6048b10268c1c8519d36538e68492815a06c818
ScherbakovMike <scherbakov.mike@gmail.com>
2023-08-21 09:29:13 +0300

    The 'TreeNode' population algorithm has been added.

--- src/main/java/LinkedInTop/TreeNode.java
+++ src/main/java/LinkedInTop/TreeNode.java
@@ -1,5 +1,7 @@
 package LinkedInTop;
 
+import java.util.ArrayDeque;
+
 public class TreeNode {
 
   public int val;
@@ -22,5 +24,39 @@ public class TreeNode {
   public TreeNode(int val, TreeNode left) {
     this.val = val;
     this.left = left;
+  }
+
+  public static TreeNode of(Integer[] nums) {
+    var queue = new ArrayDeque<TreeNode>();
+
+    TreeNode root = new TreeNode(nums[0]);
+    queue.add(root);
+
+    int i = 1;
+    while (i < nums.length) {
+      TreeNode parent = queue.poll();
+
+      Integer leftNum = nums[i++];
+
+      if (leftNum != null) {
+        TreeNode left = new TreeNode(leftNum);
+        parent.left = left;
+
+        queue.add(left);
+      }
+
+      if (i < nums.length) {
+        Integer rightNum = nums[i++];
+
+        if (rightNum != null) {
+          TreeNode right = new TreeNode(rightNum);
+          parent.right = right;
+
+          queue.add(right);
+        }
+      }
+    }
+
+    return root;
   }
 }
