7dc89730e2afd82eaa628b7e36220b6ea030b5b1
ScherbakovMike <scherbakov.mike@gmail.com>
2023-11-04 11:50:43 +0300

    The Bellman-Ford algorithm has been improved

--- src/main/java/CommonAlgorithms/ImplementBellmanFordAlgorithm.java
+++ src/main/java/CommonAlgorithms/ImplementBellmanFordAlgorithm.java
@@ -3,30 +3,34 @@ package CommonAlgorithms;
 import java.util.Arrays;
 
 public class ImplementBellmanFordAlgorithm {
+
   // The main function that finds shortest
   // distances from src to all other vertices
   // using Bellman-Ford algorithm. The function
   // also detects negative weight cycle
   // The row graph[i] represents i-th edge with
   // three values u, v and w.
-  static void BellmanFord(int[][] graph, int V, int E, int src) {
+  static void BellmanFord(int[][] graph, int nodes, int edges, int src) {
     // Initialize distance of all vertices as infinite.
-    var dis = new int[V];
-    Arrays.fill(dis, Integer.MAX_VALUE);
+    var distance = new int[nodes];
+    Arrays.fill(distance, Integer.MAX_VALUE);
 
     // initialize distance of source as 0
-    dis[src] = 0;
+    distance[src] = 0;
 
     // Relax all edges |V| - 1 times. A simple
     // shortest path from src to any other
     // vertex can have at-most |V| - 1 edges
-    for (var i = 0; i < V - 1; i++) {
+    for (var i = 0; i < nodes - 1; i++) {
+      // делаем V-1 итераций
+      for (var j = 0; j < edges; j++) {
+        var node1 = graph[j][0];
+        var node2 = graph[j][1];
+        var weight12 = graph[j][2];
+        if (distance[node1] != Integer.MAX_VALUE
+            && (distance[node1] + weight12 < distance[node2])) {
 
-      for (var j = 0; j < E; j++) {
-        if (dis[graph[j][0]] != Integer.MAX_VALUE
-            && dis[graph[j][0]] + graph[j][2] < dis[graph[j][1]]) {
-
-          dis[graph[j][1]] = dis[graph[j][0]] + graph[j][2];
+          distance[node2] = distance[node1] + weight12;
         }
       }
     }
@@ -36,18 +40,19 @@ public class ImplementBellmanFordAlgorithm {
     // distances if graph doesn't contain
     // negative weight cycle. If we get a
     // shorter path, then there is a cycle.
-    for (var i = 0; i < E; i++) {
-      var x = graph[i][0];
-      var y = graph[i][1];
-      var weight = graph[i][2];
-      if (dis[x] != Integer.MAX_VALUE && dis[x] + weight < dis[y]) {
+    for (var i = 0; i < edges; i++) {
+      var node1 = graph[i][0];
+      var node2 = graph[i][1];
+      var weight12 = graph[i][2];
+      if (distance[node1] != Integer.MAX_VALUE && distance[node1] + weight12 < distance[node2]) {
         System.out.println("Graph contains negative weight cycle");
+        break;
       }
     }
 
     System.out.println("Vertex Distance from Source");
-    for (var i = 0; i < V; i++) {
-      System.out.println(i + "\t\t" + dis[i]);
+    for (var i = 0; i < nodes; i++) {
+      System.out.println(i + "\t\t" + distance[i]);
     }
   }
 
@@ -60,10 +65,14 @@ public class ImplementBellmanFordAlgorithm {
     // the edge is from vertex u to v. And weight
     // of the edge is w.
     var graph = new int[][]{
-        {0, 1, -1}, {0, 2, 4},
-        {1, 2, 3}, {1, 3, 2},
-        {1, 4, 2}, {3, 2, 5},
-        {3, 1, 1}, {4, 3, -3}
+        {0, 1, -1},
+        {0, 2, 4},
+        {1, 2, 3},
+        {1, 3, 2},
+        {1, 4, 2},
+        {3, 2, 5},
+        {3, 1, 1},
+        {4, 3, -3}
     };
 
     BellmanFord(graph, vertices, edges, 0);
