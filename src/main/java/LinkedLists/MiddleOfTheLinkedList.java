cc515f59a8640e7830e23ba9e96fe9975206f127
ScherbakovMike <scherbakov.mike@gmail.com>
2023-11-14 17:52:57 +0400

    The task "Middle of the LinkedList" has been solved.

---
+++ src/main/java/LinkedLists/MiddleOfTheLinkedList.java
@@ -0,0 +1,20 @@
+package LinkedLists;
+
+public class MiddleOfTheLinkedList {
+
+  public ListNode middleNode(ListNode head) {
+    var fast = head;
+    var slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    return slow;
+  }
+
+  public static void main(String[] args) {
+    var list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    //var list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+    System.out.println(new MiddleOfTheLinkedList().middleNode(list).val);
+  }
+}
