eb59e3159f72f8760182bd6e801fd6e7cfbd9cf4
ScherbakovMike <scherbakov.mike@gmail.com>
2023-11-20 11:44:13 +0300

    The task "Longest Common Prefix" has been solved using sorting.

--- src/main/java/Strings/LongestCommonPrefix.java
+++ src/main/java/Strings/LongestCommonPrefix.java
@@ -1,36 +1,57 @@
 package Strings;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class LongestCommonPrefix {
 
+//  public String longestCommonPrefix(String[] strs) {
+//    var result = "";
+//    var curPos = 0;
+//    var finish = false;
+//    while (!finish) {
+//      char curChar = 0;
+//      for (var curStr : strs) {
+//        if (curPos == curStr.length()) {
+//          finish = true;
+//          break;
+//        }
+//        if (curChar == 0) {
+//          curChar = curStr.charAt(curPos);
+//        } else {
+//          if (curStr.charAt(curPos) != curChar) {
+//            finish = true;
+//            break;
+//          }
+//        }
+//      }
+//      if (!finish) {
+//        result += String.valueOf(curChar);
+//        curPos++;
+//      }
+//    }
+//    return result;
+//  }
+
   public String longestCommonPrefix(String[] strs) {
-    var result = "";
-    var curPos = 0;
-    var finish = false;
-    while (!finish) {
-      char curChar = 0;
-      for (var curStr : strs) {
-        if (curPos == curStr.length()) {
-          finish = true;
-          break;
-        }
-        if (curChar == 0) {
-          curChar = curStr.charAt(curPos);
-        } else {
-          if (curStr.charAt(curPos) != curChar) {
-            finish = true;
-            break;
-          }
+    var result = new StringBuilder();
+    Arrays.sort(strs, Comparator.comparingInt(String::length));
+    result.append(strs[0]);
+    for (var i = 1; i < strs.length; i++) {
+      var newShortestLength = result.length();
+      for (var j = result.length() - 1; j >= 0; j--) {
+        if (result.charAt(j) != strs[i].charAt(j)) {
+          newShortestLength = j;
         }
       }
-      if (!finish) {
-        result += String.valueOf(curChar);
-        curPos++;
-      }
+      result.setLength(newShortestLength);
     }
-    return result;
+    return result.toString();
   }
 
   public static void main(String[] args) {
-    System.out.println(new LongestCommonPrefix().longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+    var strs = new String[]{"cir", "car"};
+    //var strs = new String[]{"flower", "flow", "flight"};
+    System.out.println(new LongestCommonPrefix().longestCommonPrefix(strs));
   }
 }
