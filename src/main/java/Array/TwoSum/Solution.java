18de51f5700002aee0e2298684d271f4b1b1d82c
ScherbakovMike <scherbakov.mike@gmail.com>
2024-05-11 19:53:28 +0300

    Arrays. Two Sum task with Java implementation has been added.

--- src/main/java/Array/TwoSum/Solution.java
+++ src/main/java/Array/TwoSum/Solution.java
@@ -1,38 +1,22 @@
 package Array.TwoSum;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.*;
 
 public class Solution {
-  public int[] twoSum(int[] nums, int target) {
-    var origNums = Arrays.copyOf(nums, nums.length);
-    Arrays.sort(nums);
-    int maxSearchLimit = nums.length;
-    int left = 0;
-    while (left < (maxSearchLimit - 1)) {
-      int right = left + 1;
-      while (right < maxSearchLimit
-          && (target != (nums[left] + nums[right]))
-          && (target >= (nums[left] + nums[right]))
-      ) {
-        right++;
-      }
-      if ((right < maxSearchLimit) && (target == (nums[left] + nums[right]))) {
-        int finalLeft = left;
-        int origLeft = IntStream.range(0, origNums.length).filter(pos -> origNums[pos] == nums[finalLeft]).findFirst().getAsInt();
-        int finalRight = right;
-        int origRight = IntStream.range(0, origNums.length).filter(pos -> (origNums[pos] == nums[finalRight]) && (pos != origLeft)).findFirst().getAsInt();
-
-        return new int[]{origLeft, origRight};
-      }
-      left++;
+    public int[] twoSum(int[] nums, int target) {
+        var map = new HashMap<Integer, Integer>();
+        for (var i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
     }
-    throw new IllegalArgumentException();
-  }
 
-  public static void main(String[] args) {
-    var nums = new int[]{-3, 4, 3, 90};
-    var target = 0;
-    System.out.println(Arrays.toString(new Solution().twoSum(nums, target)));
-  }
+    public static void main(String[] args) {
+        var nums = new int[]{-3, 4, 3, 90};
+        var target = 0;
+        System.out.println(Arrays.toString(new Solution().twoSum(nums, target)));
+    }
 }
