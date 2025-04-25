4a7d1ea83fe7060c360d4c8004db341b340ed698
Mikhail Shcherbakov <scherbakov.mike@gmail.com>
2025-04-25 07:11:16 +0200

    A solution for the "Minimum number of moves to make palindrome" problem.

--- src/main/java/TwoPointers/MinimumNumberOfMovesToMakePandrome.java
+++ src/main/java/TwoPointers/MinimumNumberOfMovesToMakePandrome.java
@@ -1,7 +1,5 @@
 package TwoPointers;
 
-import java.util.*;
-
 /*
 2193. Minimum Number of Moves to Make Palindrome
 You are given a string s consisting only of lowercase English letters.
@@ -31,84 +29,35 @@ It can be shown that it is not possible to obtain a palindrome in less than 2 mo
 public class MinimumNumberOfMovesToMakePandrome {
 
     public static int minMovesToMakePalindrome(String s) {
-//        char[] chars = s.toCharArray();
-//        int moves = 0;
-//        int i = 0;
-//        int j = s.length() - 1;
-//        while (i < j) {
-//            if (chars[i] == chars[j]) {
-//                i++;
-//                j--;
-//                continue;
-//            }
-//            // looking the symbol in backward
-//            int indexFromRight = j;
-//            while ((indexFromRight > i) && chars[i] != chars[indexFromRight]) {
-//                indexFromRight--;
-//            }
-//            // looking the symbol in forward
-//            int indexFromLeft = i;
-//            while ((indexFromLeft < j) && chars[j] != chars[indexFromLeft]) {
-//                indexFromLeft++;
-//            }
-//            if (indexFromRight == i) {
-//                // chars[i] is the center of the odd-length string.
-//                // this char should be moved to the center
-//                int centerPosition = s.length() / 2;
-//                for (int l = i; l < centerPosition; l++) {
-//                    swap(chars, l, l + 1);
-//                    moves++;
-//                }
-//            } else {
-//                if ((indexFromLeft - i) < (j - indexFromRight)) {
-//                    for (int l = indexFromLeft; l > i; l--) {
-//                        swap(chars, l, l - 1);
-//                        moves++;
-//                    }
-//                } else {
-//                    for (int l = indexFromRight; l < j; l++) {
-//                        swap(chars, l, l + 1);
-//                        moves++;
-//                    }
-//                }
-//                i++;
-//                j--;
-//            }
-//        }
-//        return moves;
-        List<Character> chars = new ArrayList<>();
-        for (char c : s.toCharArray()) chars.add(c);
-
+        char[] chars = s.toCharArray();
         int moves = 0;
-        while (chars.size() > 1) {
-            int lastIndex = chars.size() - 1;
-
-            if (chars.get(0).equals(chars.get(lastIndex))) {
-                // already matching
-                chars.remove(lastIndex);
-                chars.remove(0);
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (chars[i] == chars[j]) {
+                i++;
+                j--;
+                continue;
+            }
+            // looking the symbol in backward
+            int matchingIndex = j;
+            while ((matchingIndex > i) && chars[i] != chars[matchingIndex]) {
+                matchingIndex--;
+            }
+            if (matchingIndex == i) {
+                // chars[i] is the center of the odd-length string.
+                // this char should be moved to the center, push it for 1 index forward
+                moves++;
+                swap(chars, i, i + 1);
             } else {
-                int k = lastIndex;
-                while (k > 0 && !chars.get(k).equals(chars.get(0))) {
-                    k--;
-                }
-
-                if (k == 0) {
-                    // No match for chars[0] — move it to center
-                    Collections.swap(chars, 0, 1);
+                for (int l = matchingIndex; l < j; l++) {
+                    swap(chars, l, l + 1);
                     moves++;
-                } else {
-                    // Bring match to the end
-                    for (int i = k; i < lastIndex; i++) {
-                        Collections.swap(chars, i, i + 1);
-                        moves++;
-                    }
-                    chars.remove(lastIndex); // matched pair
-                    chars.remove(0);
                 }
+                i++;
+                j--;
             }
         }
-
         return moves;
     }
 
@@ -119,7 +68,8 @@ public class MinimumNumberOfMovesToMakePandrome {
     }
 
     public static void main(String[] args) {
-        String s = "skwhhaaunskegmdtutlgtteunmuuludii";
+//        String s = "skwhhaaunskegmdtutlgtteunmuuludii";
+        String s = "caabb";
         System.out.println(minMovesToMakePalindrome(s));
     }
 }
