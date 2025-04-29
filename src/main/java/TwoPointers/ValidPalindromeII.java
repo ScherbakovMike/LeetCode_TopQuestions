08180a3e1a073bc8f08be48f44703d25659b44ea
ScherbakovMike <scherbakov.mike@gmail.com>
2025-04-29 10:53:24 +0200

    A solution of the task "Next Palindrome using same digits".

--- src/main/java/TwoPointers/ValidPalindromeII.java
+++ src/main/java/TwoPointers/ValidPalindromeII.java
@@ -1,35 +1,58 @@
 package TwoPointers;
 
+/*
+https://leetcode.com/problems/valid-palindrome-ii/submissions/1620879806/
+
+680. Valid Palindrome II
+Easy
+Topics
+Companies
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+
+Input: s = "abc"
+Output: false
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+ */
 public class ValidPalindromeII {
-    public static boolean isPalindrome(String string) {
-
+    public boolean validPalindrome(String string) {
         int low = 0;
-        int high = string.length() - 1;
-        int errors = 0;
-        while (low < high && errors <= 1) {
-            if (string.charAt(low) == string.charAt(high)) {
-                low++;
-                high--;
-                continue;
-            }
-
-            if (string.charAt(low + 1) == string.charAt(high)
-            ) {
-                low += 2;
-                high--;
-                errors++;
-            } else if (
-                    string.charAt(low) == string.charAt(high - 1)
-            ) {
-                high -= 2;
-                low++;
-                errors++;
-            } else return false;
+        int high = string.length()-1;
+        while(low<high && string.charAt(low)==string.charAt(high)) {
+            low++;
+            high--;
         }
-        return errors <= 1;
+        return low==high
+                || isAStrictPalindrome(string, low+1, high)
+                || isAStrictPalindrome(string, low, high-1);
+    }
+
+    private boolean isAStrictPalindrome(String string, int low, int high) {
+            while(low<high) {
+                if(string.charAt(low)!=string.charAt(high)) {
+                    return false;
+                }
+                low++;
+                high--;
+            }
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(new ValidPalindromeII().isPalindrome("ABBAB"));
+        System.out.println(new ValidPalindromeII().validPalindrome("abca"));
     }
 }
