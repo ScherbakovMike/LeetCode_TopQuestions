86b2c40284190dd73b119feeccd8f97e96393626
Mikhail Shcherbakov <scherbakov.mike@gmail.com>
2025-05-20 08:59:08 +0200

    A recursive solution for the "2130. Maximum Twin Sum of a Linked List" task has been implemented.

--- src/main/java/LinkedLists/ListNode.java
+++ src/main/java/LinkedLists/ListNode.java
@@ -1,6 +1,7 @@
 package LinkedLists;
 
 public class ListNode {
+
   public int val;
   public ListNode next;
 
@@ -14,5 +15,20 @@ public class ListNode {
   public ListNode(int val, ListNode next) {
     this.val = val;
     this.next = next;
+  }
+
+  public static ListNode of(int... arr) {
+    ListNode head = null;
+    ListNode lastNode = null;
+    for (int i = 0; i < arr.length; i++) {
+      if (head == null) {
+        head = new ListNode(arr[0]);
+        lastNode = head;
+      } else {
+        lastNode.next = new ListNode(arr[i]);
+        lastNode = lastNode.next;
+      }
+    }
+    return head;
   }
 }
