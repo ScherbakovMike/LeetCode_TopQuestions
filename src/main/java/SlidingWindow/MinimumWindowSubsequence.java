be9d7af5700d864aad3a6fd2f0ba8b8fd16923d0
Mikhail Shcherbakov <scherbakov.mike@gmail.com>
2025-06-07 11:09:29 +0200

    Remove unused IDE configuration file and add solution for "727. Minimum Window Subsequence" with initial implementation and parameterized tests.

---
+++ src/main/java/SlidingWindow/MinimumWindowSubsequence.java
@@ -0,0 +1,56 @@
+package SlidingWindow;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.stream.Stream;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/*
+https://leetcode.com/problems/minimum-window-subsequence/
+
+727. Minimum Window Subsequence
+Hard
+Topics
+Companies
+
+Given strings s1 and s2, return the minimum contiguous substring part of s1, so that s2 is a subsequence of the part.
+
+If there is no such window in s1 that covers all characters in s2, return the empty string "". If there are multiple such minimum-length windows, return the one with the left-most starting index.
+
+Example 1:
+Input: s1 = "abcdebdde", s2 = "bde"
+Output: "bcde"
+Explanation:
+"bcde" is the answer because it occurs before "bdde" which has the same length.
+"deb" is not a smaller window because the elements of s2 in the window must occur in order.
+
+Example 2:
+Input: s1 = "jmeqksfrsdcmsiwvaovztaqenprpvnbstl", s2 = "u"
+Output: ""
+
+Constraints:
+1 <= s1.length <= 2 * 104
+1 <= s2.length <= 100
+s1 and s2 consist of lowercase English letters.
+*/
+public class MinimumWindowSubsequence {
+
+  public String minWindow(String s1, String s2) {
+
+    return null;
+  }
+
+  @ParameterizedTest
+  @MethodSource("testData")
+  public void test(String s1, String s2, String expected) {
+    assertThat(new MinimumWindowSubsequence().minWindow(s1, s2), Matchers.is(expected));
+  }
+
+  public static Stream<Arguments> testData() {
+    return Stream.of(
+        Arguments.of("abcdebdde", "bde", "bcde", "jmeqksfrsdcmsiwvaovztaqenprpvnbstl", "u", ""));
+  }
+}
